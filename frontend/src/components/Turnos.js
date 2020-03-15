096e5f2378ce7f2f1562815c791e81455f7c1cfb
Lucas M. Bonomo <lucas@vgnet.com.ar>
2020-03-15 13:02:18 -0300

    Acomondo el frontend y mejorando la trancisión

--- frontend/src/components/Turnos.js
+++ frontend/src/components/Turnos.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import TableRow from '../components/TableRow';
 
-function Turnos({interval, setShow}) {
+function Turnos({ showTurnos }) {
 
-  const [date, setDate] = useState('');
-  const [time, setTime] = useState('');
   const [rows, setRows] = useState([]);
 
   async function getTurnos() {
@@ -13,46 +11,20 @@ function Turnos({interval, setShow}) {
     return data;
   }
 
-  function checkTime(i) {
-    if (i < 10) { i = "0" + i; }
-    return i;
-  }
-
-  function GetDate() {
-    let date = new Date()
-    let year = date.getFullYear();
-    let month = (1 + date.getMonth()).toString().padStart(2, '0');
-    let day = date.getDate().toString().padStart(2, '0');
-    return `${day}/${month}/${year}`;
-  }
-
-  function GetTime() {
-    let time = new Date();
-    let h = time.getHours();
-    let m = checkTime(time.getMinutes());
-    let s = checkTime(time.getSeconds());
-    return `${h}:${m}:${s}` ;
-  }
-
-  function updateDateTime() {
-    setDate(GetDate());
-    setTime(GetTime());
-  }
-
   useEffect( () => {
     if ( rows.length === 0 ) {
       // Si rows esta vacio traigo los datos (vienen despues de renderizar)
-      console.log("Primer GetRows")
-      window.setInterval(updateDateTime, 1000);
       getTurnos().then( (data) => {
         setRows(data);
-        setTimeout( () => { setShow('media') }, interval );
       });
     }
   } );
 
   return(
-    <div id="turnos" className="turnos">
+    <div
+      id="turnos"
+      className={ showTurnos ? "turnos" :  "turnos hidden"}
+      >
         <div className="header">
           <div className="net">
           {/* Mostar alerta de desconexion */}
@@ -61,8 +33,7 @@ function Turnos({interval, setShow}) {
             <h2>Agenda de turnos</h2>
           </div>
           <div className="datetime">
-            <div className="date">{date}</div>
-            <div className="time">{time}</div>
+          {/* Mostar alerta de desconexion */}
           </div>
         </div>
 
